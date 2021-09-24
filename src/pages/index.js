e4a107e244371a433a78e34099bb79844ea9500b
John <johnmpalmgren@gmail.com>
2021-09-24 12:51:18 +0100

    added framework content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,32 @@
 import * as React from "react";
 import Layout from "../components/Layout";
-
-const header = <div>
-      <h2>Web Developer</h2>
-      <a href="https://www.linkedin.com/in/john-p-6742aa40/">linkedin</a>
-      <a href="https://github.com/eniac01">github</a>
-      <a href="mailto:addemail@newemail.com">email</a>
-</div>
+import Header from "../components/homepage/Header"
+import Nav from "../components/homepage/Nav"
 
 const IndexPage = () => {
-  return <Layout pageTitle="John Palmgren" header={header}></Layout>;
+  return (
+    <Layout pageTitle="John Palmgren">
+      <Header />
+      <Nav />
+      <h3>About me</h3>
+      <p>
+        I am highly motivated web developer. I enjoy learning new programming
+        concepts and have lots of experience working with other people in
+        previous jobs. I spend a minimum of 14 hours a week developing my
+        programming skills through courses and projects alongside my day-job. I
+        have completed 15 courses in Python and web development. I am also part
+        of an online community where I give and receive feedback on coding
+        problems and projects.
+      </p>
+
+      <h3>Projects</h3>
+      <ul>
+        <li>blah</li>
+        <li>blah</li>
+        <li>blah</li>
+      </ul>
+    </Layout>
+  );
 };
 
 export default IndexPage;
