7f8a8fa0b464e8b7f1bf8d9e463e13619c59b202
John <johnmpalmgren@gmail.com>
2021-09-27 15:28:20 +0100

    'about me' styles addded

--- src/components/homepage/About.js
+++ src/components/homepage/About.js
@@ -1,21 +1,27 @@
 import React, { Fragment } from "react";
+import Card from "../UI/Card.js";
+import { subtitle, paragraph, cardWrapper } from "./About.module.css";
 
 const About = () => {
-    return (
+  return (
     <Fragment>
-      <h3>About me</h3>
-      <p>
-        I am highly motivated web developer. I enjoy learning new programming
-        concepts and have lots of experience working with other people in
-        previous jobs. I spend at least 14 hours a week developing my
-        programming skills through courses and projects alongside my day-job. I
-        have completed 15 courses in Python and web development. I am also part
-        of an online community where I give and receive feedback on coding
-        problems and projects. In January I will be attending a Bath Spa University Web 
-        Development Bootcamp. 
-      </p>
+      <h3 className={subtitle}>About me</h3>
+      <div className={cardWrapper}>
+        <Card>
+          <p className={paragraph}>
+            I am a highly motivated web developer. I enjoy learning new
+            programming concepts and have lots of experience working with other
+            people in previous jobs. I spend at least 14 hours a week developing
+            my programming skills through courses and projects alongside my
+            day-job. I have completed 15 courses in Python and web development.
+            I am also part of an online community where I give and receive
+            feedback on coding problems and projects. In January I will be
+            attending a Bath Spa University Web Development Bootcamp.
+          </p>
+        </Card>
+      </div>
     </Fragment>
-    )
-}
+  );
+};
 
-export default About
+export default About;
