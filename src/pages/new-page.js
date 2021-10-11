6f62e4f2f75146c7e56f0947a5116736ac35ab3d
John <johnmpalmgren@gmail.com>
2021-10-11 16:25:39 +0100

    changes to new page

--- src/pages/new-page.js
+++ src/pages/new-page.js
@@ -1,16 +1,1 @@
-// import * as React from "react";
-// // import Layout from "../components/Layout";
 
-// const newPage = () => {
-//   return (
-//     <Layout pageTitle="New Page">This is an example new page</Layout>
-//     // <main>
-//     //   <title>New Page</title>
-//     //   <h1>New Page</h1>
-//     //   <Link to="/">back to home</Link>
-//     //   <p>this is an example new page</p>
-//     // </main>
-//   );
-// };
-
-// export default newPage;
