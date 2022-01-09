f5d2f4db49a52ace3115f65bbb0dae61293ac36a
John <johnmpalmgren@gmail.com>
2022-01-09 08:52:45 +0000

    updated current project

--- src/components/homepage/CurrentProject.js
+++ src/components/homepage/CurrentProject.js
@@ -1,18 +1,19 @@
-import React, { Fragment } from "react"
-import { subtitle, wrapper, paragraph } from "./CurrentProject.module.css"
+import React, { Fragment } from "react";
+import { subtitle, wrapper, paragraph } from "./CurrentProject.module.css";
 
 const CurrentProject = () => {
-    return (
-      <Fragment>
-        <h3 className={subtitle}>What I'm doing now</h3>
-        <div className={wrapper}>
-        <p className={paragraph}>I'm currently using Python, Flask and React to build a 
-        bluetooth speaker system with a visual display with my raspberry pi. When I'm not doing this I'm doing
-        a deep dive into React in a Udemy course. 
+  return (
+    <Fragment>
+      <h3 className={subtitle}>What I'm doing now</h3>
+      <div className={wrapper}>
+        <p className={paragraph}>
+          I'm currently doing a web development bootcamp with Bath Spa
+          University. When I'm not doing that I'm tinkering with my Raspberry
+          Pi: building a hifi system with album art displayed on screen.
         </p>
-        </div>
-      </Fragment>
-    );
-}
+      </div>
+    </Fragment>
+  );
+};
 
-export default CurrentProject
+export default CurrentProject;
